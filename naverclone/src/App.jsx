4e7b32a641fd8dc1b331587c9506d69bcca6bc44
aquaman <swj981128@gmail.com>
2023-08-21 11:08:04 +0900

    naverClone__Header

---
+++ naverclone/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      {/* 전체 좌우 공백 */}
+      <div className='container'>
+        {/* div 3개 container */}
+        <div className='header'>
+          {/* 제일 윗줄 icons */}
+          <div className='header-icons'>
+            <div className='icon-boxes'>
+              <div className='icons'>menu</div>
+              <div className='icons'>NPay</div>
+            </div>
+            <div className='icon-boxes'>
+              <div className='icons'>comment</div>
+              <div className='icons'>alarm</div>
+            </div>
+          </div>
+          {/* 검색창 */}
+          <div className='header-search'>
+            <h1>
+              <a>icon</a>
+            </h1>
+            <form>
+              <fieldset>
+                <input type="text" className='header-search__input' />
+                <button className='header-search__button'>input equipment</button>
+                <button className='header-search__button'>dot</button>
+                <button className='header-search__button'>search</button>
+              </fieldset>
+            </form>
+          </div>
+          {/* 아이콘들 */}
+          <div>
+
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
