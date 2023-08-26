8ab6ac8ec6405d2a01b259470217a75bca4912b6
aquaman <swj981128@gmail.com>
2023-08-26 17:50:58 +0900

    .

--- naverclone/src/App.jsx
+++ naverclone/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
               </h1>
               <form>
                 <fieldset>
-                  <input type="text" className='header-search__input' />
+                  <input type="text" className='header-search__input' placeholder='검색어를 입력해 주세요.'/>
                   <button className='header-search__button'>input equipment</button>
                   <button className='header-search__button'>dot</button>
                   <button className='header-search__button'>search</button>
@@ -36,7 +36,46 @@ function App() {
           </div>
           {/* 아이콘들 */}
           <div>
+            <div>
 
+              <div>
+                <div>메일 아이콘</div>
+                <div>메일</div>
+              </div>
+              <div>
+                <div>카페 아이콘</div>
+                <div>카페</div>
+              </div>
+              <div>
+                <div>블로그 아이콘</div>
+                <div>블로그</div>
+              </div>
+              <div>
+                <div>쇼핑 아이콘</div>
+                <div>쇼핑</div>
+              </div>
+              <div>
+                <div>뉴스 아이콘</div>
+                <div>뉴스</div>
+              </div>
+              <div>
+                <div>증권 아이콘</div>
+                <div>증권</div>
+              </div>
+              <div>
+                <div>부동산 아이콘</div>
+                <div>부동산</div>
+              </div>
+              <div>
+                <div>지도 아이콘</div>
+                <div>지도</div>
+              </div>
+              <div>
+                <div>웹툰 아이콘</div>
+                <div>웹툰</div>
+              </div>
+
+            </div>
           </div>
         </div>
 
