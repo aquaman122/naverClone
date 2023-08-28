527e185c7139022608312e0ae191980d4b4caf89
aquaman <swj981128@gmail.com>
2023-08-28 12:01:43 +0900

    .

--- naverclone/src/App.jsx
+++ naverclone/src/App.jsx
@@ -35,46 +35,75 @@ function App() {
             </div>
           </div>
           {/* 아이콘들 */}
-          <div>
-            <div>
+          <div className='header-menu'>
+            <div className='menu__container'>
 
               <div>
-                <div>메일 아이콘</div>
-                <div>메일</div>
+                <div className='menu__icons'>메일</div>
+                <div className='menu__name'>메일</div>
               </div>
               <div>
-                <div>카페 아이콘</div>
-                <div>카페</div>
+                <div className='menu__icons'>카페</div>
+                <div className='menu__name'>카페</div>
               </div>
               <div>
-                <div>블로그 아이콘</div>
-                <div>블로그</div>
+                <div className='menu__icons'>블로그</div>
+                <div className='menu__name'>블로그</div>
               </div>
               <div>
-                <div>쇼핑 아이콘</div>
-                <div>쇼핑</div>
+                <div className='menu__icons'>쇼핑</div>
+                <div className='menu__name'>쇼핑</div>
               </div>
               <div>
-                <div>뉴스 아이콘</div>
-                <div>뉴스</div>
+                <div className='menu__icons'>뉴스</div>
+                <div className='menu__name'>뉴스</div>
               </div>
               <div>
-                <div>증권 아이콘</div>
-                <div>증권</div>
+                <div className='menu__icons'>증권</div>
+                <div className='menu__name'>증권</div>
               </div>
               <div>
-                <div>부동산 아이콘</div>
-                <div>부동산</div>
+                <div className='menu__icons'>부동산</div>
+                <div className='menu__name'>부동산</div>
               </div>
               <div>
-                <div>지도 아이콘</div>
-                <div>지도</div>
+                <div className='menu__icons'>지도</div>
+                <div className='menu__name'>지도</div>
               </div>
               <div>
-                <div>웹툰 아이콘</div>
-                <div>웹툰</div>
+                <div className='menu__icons'>웹툰</div>
+                <div className='menu__name'>웹툰</div>
               </div>
+              
+            </div>
+          </div>
+          {/* 아이콘 끝 */}
+        </div>
+        
+        <div className='layout-container'>
+          <div className='layout-left__column'>
+            <div className='left__ad'>
+              <img src="" alt="" />
+            </div>
+            {/* 뉴스 */}
+            <div className='left__news'>
+              <div className='news__header'>
+                <a href="/">뉴스스탠드</a>
+                <a href="/">+ 언론사편집</a>
+                <a href="/">/ 엔터</a>
+                <a href="/">/ 스포츠</a>
+                <a href="/">/ 경제</a>
+              </div>
+              <div className='news__haeder__preview'>
 
+              </div>
+            </div>
+            {/* 뉴스 끝 */}
+          </div>
+
+          <div className='layout-right__column'>
+            <div className='right__login'>
+              
             </div>
           </div>
         </div>
